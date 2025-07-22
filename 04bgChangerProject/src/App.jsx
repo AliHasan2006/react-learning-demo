477ae1b17539bf53adbbbbe171260396c03e10fa
AliHasan@2006 <alihasan72@gmail.com>
2025-07-22 17:13:10 +0530

    This is Minor Change in Project 4

--- 04bgChangerProject/src/App.jsx
+++ 04bgChangerProject/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
+import React, { useState, useCallback } from 'react';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const memoizedHandleClick = useCallback(() => {
+    console.log("Button clicked!");
+  }, []); // Empty array = function recreate nahi hoga
 
   return (
-    <>
-    <h1 className="w-full h-screen duration-200">
-    
-    </h1>
-    </>
-  )
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>+ Increase</button>
+      <Child handleClick={memoizedHandleClick} />
+    </div>
+  );
 }
-
-export default App
+export default App;
