7c79b55bea50ae2915993ad70a3791920217e6d7
AliHasan@2006 <alihasan72@gmail.com>
2025-07-23 20:51:17 +0530

    Project Five is finally completed

--- 04bgChangerProject/src/App.jsx
+++ 04bgChangerProject/src/App.jsx
@@ -3,18 +3,14 @@ import React, { useState, useCallback } from 'react';
 
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const memoizedHandleClick = useCallback(() => {
-    console.log("Button clicked!");
-  }, []); // Empty array = function recreate nahi hoga
 
   return (
-    <div>
-      <h2>Count: {count}</h2>
-      <button onClick={() => setCount(count + 1)}>+ Increase</button>
-      <Child handleClick={memoizedHandleClick} />
-    </div>
+    <>
+     <h1 className="text-3xl font-bold underline text-center">
+      Hello world!
+    </h1>
+    </>
+    
   );
 }
 export default App;
